df5faa0af781355c49135a2e7e5f442e0e577efd
Sunit Corneleous <sunit.corneleous25@gmail.com>
2022-10-12 23:35:09 +0600

    phone by id

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ app.get("/phones", (req, res) => {
   res.send(phones);
 });
 
+app.get("/phones/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const phone = phones.find(ph => ph.id === id) || {};
+
+  res.send(phone);
+});
+
 app.listen(port, () => {
   console.log(`This server is listening on port ${port}`);
 });
